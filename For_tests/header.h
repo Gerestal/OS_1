88be07dc8e8106446dc2d90551676bbcc3b491ba
Kseniya Maslenchenko <ksjusha.mas@gmail.com>
2025-09-29 16:52:20 +0300

    Лишнее

--- For_tests/header.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <string>
-#include <istream>
-#include <ostream>
-
-std::string ReadBinaryFileName(std::istream& in, std::ostream& out);
-int ReadRecordCount(std::istream& in, std::ostream& out);
